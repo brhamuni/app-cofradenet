3a20d495b074463227f85808022588c72bab837e
Abraham García Hurtado <brham.uni@gmail.com>
2026-06-05 20:31:39 +0200

    fix: dependences

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import Header from "../components/header/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
